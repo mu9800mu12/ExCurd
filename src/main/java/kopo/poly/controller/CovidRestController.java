ae6c19a065d8dae5df22544308e5ca6a4a1b4fbd
data22 <mu9800mu12@naver.com>
2024-01-21 14:48:52 +0900

    코로나API 서비스 및 컨트롤러 완!

---
+++ src/main/java/kopo/poly/controller/CovidRestController.java
@@ -0,0 +1,26 @@
+package kopo.poly.controller;
+
+import kopo.poly.service.ICovidService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/covid")
+public class CovidRestController {
+
+    private final ICovidService convidService;
+
+
+    @GetMapping("apiCovid")
+    public String mian()throws Exception {
+        log.info(this.getClass().getName() + "");
+
+        String result = covidService.getCovidRes();
+
+        return result;
+
+    }
+}
--- src/main/java/kopo/poly/service/impl/CovidService.java
+++ src/main/java/kopo/poly/service/impl/CovidService.java
@@ -4,18 +4,26 @@ import kopo.poly.service.ICovidService;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+//import java.net.http.HttpHeaders;
+
+import org.springframework.http.HttpHeaders;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service("CovidService")
 public class CovidService implements ICovidService {
-//
+
     private final RestTemplate restTemplate;
 
-    @Value("${api.service.key")
+    @Value("${api.service.key}")
     private String apikey;
 
     @Override
@@ -30,5 +38,19 @@ public class CovidService implements ICovidService {
                 .build().toString();
 
         log.info("생성된 ur 확인 :" + url);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity entity = new HttpEntity(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        log.info("api 조회 결과는? : " + response.getStatusCode());
+        log.info("api 조회 결과 헤더에 담긴 정보는? : " + response.getHeaders());
+        log.info("api 조회 실제 데이터는 : " + response.getBody());
+
+        log.info(this.getClass().getName() + "코로나 확진자 정보 가져오기 종료!");
+        return response.getBody();
     }
 }
